f411875e89329c0de422da32c4d3b6925b45da66
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-06-29 00:21:28 +0800

    feat: interval, urlInterval

--- src/config/settings.rs
+++ src/config/settings.rs
@@ -1,11 +1,27 @@
 use std::fs;
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct Settings {
+    #[serde(default)]
     pub proxy: Option<String>,
+
     pub urls: Vec<String>,
+
+    #[serde(default = "default_interval")]
+    pub interval: u64,
+
+    #[serde(default = "default_url_interval")]
+    pub url_interval: u64,
 }
 
+fn default_interval() -> u64 {
+    5
+}
+
+fn default_url_interval() -> u64 {
+    0
+}
 
 pub fn load_config() -> Settings {
     let settings = fs::read_to_string("config.yml").expect("Unable to read config file");
--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,40 @@ mod proxy;
 
 use reqwest::Client;
 use std::time::Duration;
-use tokio::time::interval;
-
+use tokio::time::Interval;
 
 #[tokio::main]
 async fn main() {
     let cfg = config::settings::load_config();
-    let mut interval = interval(Duration::from_secs(5));
+
+    let mut global_tick: Option<Interval> = if cfg.interval > 0 {
+        Some(tokio::time::interval(Duration::from_millis(cfg.interval)))
+    } else {
+        None
+    };
+    let mut per_url_tick: Option<Interval> = if cfg.url_interval > 0 {
+        Some(tokio::time::interval(Duration::from_millis(cfg.url_interval)))
+    } else {
+        None
+    };
 
     let mut client_builder = Client::builder();
 
     if let Some(proxy_url) = cfg.proxy {
-        if let Ok(proxy) = proxy::builder::build_proxy(&proxy_url) {
+        if !proxy_url.eq("direct") && let Ok(proxy) = proxy::builder::build_proxy(&proxy_url) {
             client_builder = client_builder.proxy(proxy);
+        } else {
+            client_builder = client_builder.no_proxy();
         }
     }
-    
+
     let client = client_builder.build().expect("failed to build client");
 
     loop {
-        interval.tick().await;
-
+        if let Some(tick) = global_tick.as_mut() { tick.tick().await; }
         for url in &cfg.urls {
+            if let Some(tick) = per_url_tick.as_mut() { tick.tick().await; }
+
             match client.get(url).send().await {
                 Ok(resp) if resp.status().is_success() => {
                     println!("{} => SUCCESS", url);
